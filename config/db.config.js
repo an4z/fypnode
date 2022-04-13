4a3b05bbf717b6c6a2c19c395c02e85e79854fe0
an4z <an4z@outlook.com>
2022-04-14 02:32:22 +0545

    added db connection

--- config/db.config.js
+++ config/db.config.js
@@ -2,12 +2,12 @@ const mysql = require('mysql');
 
 // create here mysql connection
 
-const dbConn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'api'
-});
+// const dbConn = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'api'
+// });
 
 // const dbConn = mysql.createConnection({
 //     host: 'taalika.chbkfqrbx6yy.ap-south-1.rds.amazonaws.com',
@@ -15,6 +15,13 @@ const dbConn = mysql.createConnection({
 //     password: 'mypassword',
 //     database: 'taalika'
 // });
+
+const dbConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'taalikaa_anuz',
+    password: 'Burner123!@',
+    database: 'taalikaa_database'
+});
 
 dbConn.connect(function(error){
     if(error) throw error;
